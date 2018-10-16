9498b9ffd90a7496f026f826ae50c6df90559c94
Bailey Diveley <bailey.diveley@gmail.com>
2018-10-16 11:41:28 -0600

    Add routes

--- app/controllers/wild/cats_controller.rb
+++ app/controllers/wild/cats_controller.rb
@@ -1,5 +1,13 @@
 class Wild::CatsController < ApplicationController
+
   def index
     @cats = Cat.where(status: 1)
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    if @cat.status != 'wild'
+      redirect_to wild_cats_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
 
+ resources :owners do
+   resources :cats, only: [:index, :show]
+ end
+
+ namespace :wild do
+   resources :cats
+ end 
 end
